2de641e3e0ee658a90b2ae7c4b9f33dff1b724ea
mariogutierrez99 <kiqueguti14@gmail.com>
2019-04-14 15:28:49 -0600

    WebQuest

---
+++ git.kt
@@ -0,0 +1,57 @@
+/*
+1. ¿Qué es un control de versiones?
+
+Se llama control de versiones a la gestión de los diversos cambios que se realizan 
+sobre los elementos de algún producto o una configuración del mismo. Una versión, 
+revisión o edición de un producto, es el estado en el que se 
+encuentra el mismo en un momento dado de su desarrollo o modificación.
+
+2. ¿Cuáles son las ventajas de utilizar un controlador de versiones?
+
+- Acceso al registro de modificaciones sobre un fichero. 
+Esto nos permite además ver los comentarios asociados a cada 
+modificación y la persona que ha realizado dicha modificación
+- Posibilidad de regresar al estado anterior de un fichero o conjunto de ficheros
+- Creación de ramas para gestionar cambios que finalmente se mezclarán con la rama principal
+- Facilidad para etiquetar modificaciones concretas. Esto además, acaba desembocando 
+en un DML de ITIL, con lo que conseguimos un repositorio con todas las releases del producto
+
+3. ¿Cuáles son los controladores de versiones disponibles para android studio?
+
+-GitLab
+-GitHub
+-BitBucket
+-Azure DevOps
+
+4. ¿Cuáles son los servidores de controladores de versiones mas conocidos?
+
+    -Git 
+    -CVS
+    -Mercurial
+    -Subversion
+    -Bazaar
+
+5. ¿Investigue los siguientes terminos Push, Commit, Branch, Delete, Stack y Repositorio?
+
+    -Push: Es un comando que sube cambios hechos en tu ambito de trabajo a una rama de trabajo
+    de mi ordenanador y/o de tu equipo remota.
+
+    -Commit: Es la acción de guardar o subir tus archivos a tu repositorio remoto (es una 
+    actualización de tus cambios) también puede hacerse al local (depende donde hayas creado 
+    tu repositorio).
+
+    -Branch: Es cuando se van a llevar a cabo cambios importantes que romperán 
+    la compilación, pruebas, experimentos o intentos de optimización, debe crearse 
+    una nueva rama de desarrollo, con la que no molestemos a los compañeros,
+    esto es un branch: una copia del código o la rama de la que deriva. .
+
+    -Repositorio: Es el lugar en el que se almacenan los datos actualizados e históricos
+    de cambios, a menudo en un servidor. A veces se le denomina depósito o depot. 
+    Puede ser un sistema de archivos en un disco duro, un banco de datos, etc.. 
+
+    -Stack: es una lista ordenada o estructura de datos que permite almacenar y 
+    recuperar datos, el modo de acceso a sus elementos es de tipo LIFO 
+    (del inglés Last In, First Out, «último en entrar, primero en salir»). 
+    Esta estructura se aplica en multitud de supuestos en el área de informática 
+    debido a su simplicidad y capacidad de dar respuesta a numerosos procesos.
+ */
